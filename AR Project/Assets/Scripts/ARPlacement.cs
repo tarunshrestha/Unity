29c4bbc5ce91da2f3f4e6ce6a417137adb7cb7f0
tarunshrestha <tarunshrestha90@gmail.com>
2024-07-21 14:46:29 +0545

    AR

---
+++ AR Project/Assets/Scripts/ARPlacement.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ARPlacement : MonoBehaviour
+{
+    public GameObject arObjectToSpawn;
+    public GameObject placementIndicator;
+    GameObject spawnObject;
+    Pose PlacementPose;
+    ARRaycastManager aRRaycastManager;
+    bool placementIsValid = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        aRRaycastManager = FindObjectOfType<ARRaycastManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //To place object
+        if(spawnObject == null && placementIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            ARPlaceObject();
+        }
+
+        //To Track placement
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+        
+    }
+
+    private void UpdatePlacementIndicator()
+    {
+        if(spawnObject == null && placementIsValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(PlacementPose.position, PlacementPose.rotation);
+        }        
+    }
+
+    private void UpdatePlacementPose()
+    {
+        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();
+        aRRaycastManager.Raycast(screenCenter, hits, TrackableType.Planes);
+
+        placementIsValid = hits.Count > 0;
+        if (placementIsValid)
+        {
+            PlacementPose = hits[0].pose;
+        }
+    }
+
+    void ARPlaceObject()
+    {
+        spawnObject = Instantiate(arObjectToSpawn, PlacementPose.position, PlacementPose.rotation);
+    }
+}
---
+++ AR Project/Assets/Scripts/ObjectController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectController : MonoBehaviour
+{
+    public GameObject Player;
+    Animator anim;
+    bool canjump = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Jump()
+    {
+        if (canjump)
+        {
+            //GetComponent<Rigidbody2D>().velocity = new Vector3(0, 5, 0);
+            anim.SetTrigger("Jump");
+            canjump = false;
+        }
+    }
+}
